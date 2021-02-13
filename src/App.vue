eef1b51e6a798726515deaefa1d4cea31719d30d
Sebastian Isaza <sebasisaza_27@hotmail.com>
2021-02-13 12:12:49 -0500

    ajustes front base

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,60 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">General</router-link> |
+    <header>
+      <h1>Control de Gastos</h1>
+    </header>
+    <nav>
+      <router-link to="/">General</router-link>
       <router-link to="/expense-control">Control de Gastos</router-link>
-    </div>
+      <router-link to="/day">Cobros día</router-link>
+      <router-link to="/customers">Clientes</router-link>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap');
 *{
   margin: 0;
   padding: 0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: 'Poppins', sans-serif;
 }
 
-#nav {
-  padding: 30px;
+a:link{
+  text-decoration: none;
+  color: #101A26;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+header{
+  background: #101A26;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+nav {
+  padding: 0.3rem;
+  background: #ccc;
+  color: #fff;
+  display: flex;
+  justify-content: space-around;
+}
+
+nav a{
+  padding: 10px;
+  text-decoration: none;
+  font-weight: bolder;
+}
+
+nav a:visited{
+  text-decoration: none;
+  color: #101A26;
+}
+
+nav a.router-link-exact-active {
+  color: #fff;
 }
 </style>
